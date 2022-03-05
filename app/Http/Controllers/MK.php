a5121a0e5c8b50e4683b16fec4f03d452356fa6c
nour <nanoshe.247@gmail.com>
2022-03-05 13:31:14 -0500

    first commit

---
+++ app/Http/Controllers/MK.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MK extends Controller
+{
+    //
+    public function blabla(){
+
+        $obj1= new \stdClass();
+        $obj1 -> name='bla bka bka ';
+        $obj1 -> age= 17;
+
+        return view('mywelcome',compact('obj1'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\fcontroller;
+use app\Http\Controllers\MK;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('first',fcontroller::class);
+//Route::resource('first',fcontroller::getIndex());
+route::get('view','MK@blabla');
